f6dc6d2a5527e33b5ffa5b03f168a627178cff60
Db1zz <xel27@inbox.lv>
2025-01-21 14:14:49 +0100

    move delete[] to the main

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -1,7 +1,7 @@
 #include "Zombie.hpp"
 #include <iostream>
 
-void announceAndKillZombies(int N, Zombie* zombies) {
+void announceZombies(int N, Zombie* zombies) {
   if (!zombies) {
     return;
   }
@@ -9,12 +9,12 @@ void announceAndKillZombies(int N, Zombie* zombies) {
   for (int i = 0; i < N; i++) {
     zombies[i].announce();
   }
-  delete[] zombies;
 }
 
 int main() {
   const int zombies_amount = 10;
   Zombie* zombies = zombieHorde(zombies_amount, "Abobus");
-  announceAndKillZombies(zombies_amount, zombies);
+  announceZombies(zombies_amount, zombies);
+  delete[] zombies;
   return 0;
 } 
