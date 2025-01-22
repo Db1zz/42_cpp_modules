f5d1712c175d8c697513e0196770f3be5eedf72a
Db1zz <xel27@inbox.lv>
2025-01-22 14:04:42 +0100

    fix Linux -werror bug

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -7,6 +7,7 @@ void Harl::debug() {
             << "I love having extra bacon for my "
                "7XL-double-cheese-triple-pickle-specialketchup burger. I "
                "really do!\n\n";
+  info();
 }
 
 void Harl::info() {
@@ -14,6 +15,7 @@ void Harl::info() {
             << "I cannot believe adding extra bacon costs more money. You "
                "didn’t put enough bacon in my burger! If you did, I wouldn’t "
                "be asking for more!\n\n";
+  warning();
 }
 
 void Harl::warning() {
@@ -21,6 +23,7 @@ void Harl::warning() {
             << "I think I deserve to have some extra bacon for free. I’ve been "
                "coming for years whereas you started working here since last "
                "month.\n\n";
+  error();
 }
 
 void Harl::error() {
@@ -32,7 +35,7 @@ void Harl::unknown() {
   std::cout << "[ Probably complaining about insignificant problems ]\n";
 }
 
-Harl::_state Harl::levelToState(const std::string &level) {
+Harl::_state Harl::levelToState(const std::string& level) {
   const static std::string states[] = {
       "DEBUG", "INFO", "WARNING", "ERROR"};
   for (int i = 0; i < static_cast<int>(TOTAL); i++) {
@@ -43,16 +46,19 @@ Harl::_state Harl::levelToState(const std::string &level) {
   return TOTAL;
 }
 
-void Harl::complain(const std::string &level) {
+void Harl::complain(const std::string& level) {
   _state state = levelToState(level);
 
   switch (state) {
   case DEBUG:
     debug();
+    break;
   case INFO:
     info();
+    break;
   case WARNING:
     warning();
+    break;
   case ERROR:
     error();
     break;
