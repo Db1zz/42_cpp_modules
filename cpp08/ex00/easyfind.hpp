2f0c60452759e6948576e6d6f7e5381b1c3a89c3
Db1zz <xel27@inbox.lv>
2025-02-13 15:48:56 +0100

    ex00 is done

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <algorithm>
+#include <exception>
+#include <iostream>
+
+class ObjectNotFound : public std::exception {
+public:
+  const char *what() const throw() {
+    return "std::find failed to find a value in the container";
+  }
+};
+
+template <typename T>
+void easyfind(const T &container, int value) {
+  typename T::const_iterator it = std::find(container.begin(), container.end(), value);
+  if (it == container.end())
+    throw ObjectNotFound();
+  std::cout << "easyfind() just found value: " << value << std::endl;
+}
+
+#endif // EASYFIND_HPP
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,40 @@
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
+#include <deque>
+#include <iostream>
+
+template <typename T, typename U>
+T &operator<<(T &v, const U &item) {
+  v.push_back(  item);
+  return v;
+}
+
+template <typename T, typename U>
+T &operator,(T &v, const U &item) {
+  v.push_back(item);
+  return v;
+}
+
+int main() {
+  std::vector<int> values; values << 0, 1, 2, 3, 4;
+  std::list<int> list; list << 0, 1, 2, 3, 4;
+  std::deque<int> deque; deque << 0, 1, 2, 3, 4;
+
+  int to_find1 = 4;
+  int to_find2 = 5;
+
+  try {
+    easyfind(list, to_find1);
+    easyfind(deque, to_find1);
+    easyfind(values, to_find1);
+
+    // These easyfind() calls should throw an exception.
+    easyfind(list, to_find2);
+    // easyfind(deque, to_find2);
+    // easyfind(values, to_find2);
+  } catch (const std::exception &e) {
+    std::cout << "Exception: " << e.what() << std::endl;
+  }
+  return 0;
+}
