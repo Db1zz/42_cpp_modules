4a3f28cd855d98ee2f5b585d8dd0dc416ab19b24
Db1zz <xel27@inbox.lv>
2025-02-26 14:07:31 +0100

    make it linux compatible

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -7,61 +7,62 @@
 template <typename T>
 class Array {
 public:
-  Array() : data_(NULL), size_(0) {}
-  Array(uint32_t n) {
+  Array(u_int32_t n = 0)
+    : data_(NULL), size_(n)
+  {
+    std::cout << "Default Array constructor called" << std::endl;
+    if (size_ > 0) {
       data_ = new int[n];
       if (!data_) {
         return;
       }
-      for (uint32_t i = 0; i < n; i++) {
+      for (u_int32_t i = 0; i < size_; i++) {
         data_[i] = 0;
       }
-      size_ = n;
+    }
   }
 
-  Array(const Array<T> &copy) {
+  Array(const Array<T> &copy)
+    : data_(NULL), size_(0)
+  {
+    std::cout << "Array copy constructor called" << std::endl;
     *this = copy;
   }
 
   ~Array() {
+    std::cout << "Array destructor called" << std::endl;
     if (data_) {
       delete[] data_;
     }
   }
 
-  uint32_t size() const {return size_;}
-
-  class IndexOutOfBounds : public std::exception {
-    public:
-      const char *what() const throw() {
-        return "Index is out of bounds";
-      }
-  };
+  u_int32_t size() const {return size_;}
 
   Array<T> &operator=(const Array<T> &to_copy) {
-    if (this == &to_copy) {
-      return *this;
-    } else if (data_ == NULL) {
-      delete[] data_;
-    }
-    data_ = new T[to_copy.size_];
-    size_ = to_copy.size_;
-    for (uint32_t i = 0; i < to_copy.size_; i++) {
-      data_[i] = to_copy.data_[i];
+    std::cout << "Array copy asignment operator called" << std::endl;
+    if (this != &to_copy) {
+      if (data_ == NULL) {
+        delete[] data_;
+      }
+      data_ = new T[to_copy.size_];
+      size_ = to_copy.size_;
+      for (u_int32_t i = 0; i < to_copy.size_; i++) {
+        data_[i] = to_copy.data_[i];
+      }
     }
     return *this;
   }
 
-  T &operator[](int64_t index) {
-    if (!(size_ > 0 && index < size_)) {
-      throw Array::IndexOutOfBounds();
+  T &operator[](u_int32_t index) {
+    if (index >= size_) {
+      throw std::runtime_error("Index is out of bounds");
     }
     return data_[index];
   }
 
 private:
   T *data_;
-  uint32_t size_;
+  u_int32_t size_;
 };
 
 #endif  // ARRAY_HPP
