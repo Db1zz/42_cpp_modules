8ed8b2e495ccdb7a361e9db9c45eda49d1df5a1c
Db1zz <xel27@inbox.lv>
2025-03-05 15:02:19 +0100

    add copy_interval func()

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -7,3 +7,20 @@ public:
 };
 
 #endif  // P_MERGE_ME_HPP
+
+
+// Parse
+
+
+/*
+	1. Parser
+	2. Camera/Viewport
+	3. Objects
+	4. Light system
+
+	Cylinder, plane, sphere | hyperboloid
+
+	Multiple light sources(with color)
+
+	textures | bump maps
+*/
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -48,17 +48,24 @@ bool is_pair_greater(std::vector<int> &main, long p1_start, long p2_start, long 
   return (main[p1_start + pair_size - 1] > main[p2_start + pair_size - 1]);
 }
 
+template <typename T>
+void copy_interval(
+  const T &src, T &dest, long interval_size, bool start_from_interval)
+{
+  const long size = src.size();
+  long i = 0;
+
+  if (start_from_interval) {
+    i += interval_size;
+  }
+  while (i + interval_size <= size) {
+    std::cout << i << std::endl;
+    std::copy(src.begin() + i, src.begin() + i + interval_size, std::back_inserter(dest));
+    i += interval_size * 2;
+  }
+}
 
 /*
-  11,2, 17,0, 16,8, 6,15, 10,3, 21,1, 18,9, 14,19, 12,5, 4,20, 13,7
-  -- --
-  
-
-  11,2, 17,0, 16,8, 6,15, 10,3, 21,1, 18,9, 14,19, 12,5, 4,20, 13,7
-  ----  ----
-
-  this function is the real banger >_<
-
   References:
     https://warwick.ac.uk/fac/sci/dcs/teaching/material-archive/cs341/fj.pdf
     https://dev.to/emuminov/human-explanation-and-step-by-step-visualisation-of-the-ford-johnson-algorithm-5g91 
@@ -76,69 +83,14 @@ void sort_pairs(std::vector<int> &main, const long pair_size) {
     }
   }
   sort_pairs(main, pair_size * 2);
-  /*
-    Oke, Oke. Now we have to implement binary search insertion with jacobsthal numbers.
 
-    I didn't understand the explanation from the Donald Knuth, so for this case I'm using this reference:
-    https://dev.to/emuminov/human-explanation-and-step-by-step-visualisation-of-the-ford-johnson-algorithm-5g91
-  */
-  /*
-    step 2:
-    pend is the rest of the B's
-    main is the b1 and the rest of the A's
-    step 3:
-      if (go to the step 4 if there's not enough elements to perform the insertion in the Jacobsthal numbers orderd)
-        go to step 4
-      else
-        perform 
+  int perv = 1;
+  std::vector<int> new_main; // b0 + a0...ax
+  std::vector<int> pend; // b1 + bx
 
-    step 4:
-      green is the area of inserttion
-      and red is the bound or the end of the search.
+  while (true) {
 
-      the current element needs to be with other elements in search area and insert
-      it in the corresponding place. eg: elements b1 and a1 with 16, 17 and
-      element that we want to insert is 19 so we need to insert it like this:
-      16, 17, 19
-
-    inserting odd element:
-    Since we don't have any guarantees that this odd element is sorted, 
-    we have to compare it with the whole array.
-    using binary search we need to compare it and insert.
-  */
-
-
-  /*
-    (b0, a0) (b1, a1) (b2, a3) (b4, a4) (b5, a5)
-
-    b0 is always smaller than a0, so we don't need to threat the pair as insertable or pending
-    pairs from b1....bn are insertable or pending
-
-    so the correct amount of pending can be calculated by: (pair_amount - 2) / 2
-  */
-  if (pair_amount > 2) {
-    int pending = (pair_amount - 2) / 2;
-    int perv = 1;
-    for (long i = 2; i < pair_amount; i++) {
-      int curr = jacobsthal_numbers(pending) - perv; // the number is the amount of pairs
-      pending -= curr;
-      perv = curr;
-      std::vector<int> p(main.begin() + i * pair_size, main.begin() + i * (pair_size * 2));
-      while (perv != curr) {
-        int curr_index = pair_size * 2 * i - 1;
-        if (curr_index >= main.size()) {
-          break;
-        }
-        std::binary_search(main.begin(), main.begin() + pair_size, p, std::greater);
-        --curr;
-      }
-    }
   }
-  /*
-    curr - perv
-  */
-  // if (/* check if there's enough pairs to sort with Jacobsthal insertion*/)
-  // else /* perform stupid binarysearch insertion */
 }
 
 void merge_insertion_sort(std::vector<int> &numbers)
@@ -151,10 +103,13 @@ void merge_insertion_sort(std::vector<int> &numbers)
 }
 
 int main(int ac, char **av) {
-  // const long size = 22;
-  // int numbers[] = {11,2, 17,0, 16,8, 6,15, 10,3, 21,1, 18,9, 14,19, 12,5, 20,4, 13,7};
-  // std::vector<int> input(&numbers[0], &numbers[size]);
-  // merge_insertion_sort(input);
-  // std::cout << jacobsthal_numbers(std::atoi(av[1])) << std::endl;
+  const long size = 22;
+  int numbers[] = {11,2, 17,0, 16,8, 6,15, 10,3, 21,1, 18,9, 14,19, 12,5, 20,4, 13,7};
+  std::vector<int> input(&numbers[0], &numbers[size]);
+  merge_insertion_sort(input);
+
+  // std::vector<int> dest;
+  // copy_interval(input, dest, 4, true);
+  // display_array(dest);
   return 0;
 }
