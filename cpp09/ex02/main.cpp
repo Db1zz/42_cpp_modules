89fffa219f059d3ccf63776e7956a8b90d5cc51f
Db1zz <xel27@inbox.lv>
2025-03-06 18:23:15 +0100

    it works with the simple set of numbes

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -19,6 +19,16 @@ void display_array(const std::vector<int> &array) {
   std::cout << "]" << std::endl;
 }
 
+void display_matrix(const std::vector<std::vector<int> > &array) {
+  std::cout << "[";
+  for (size_t i = 0; i < array.size(); i++) {
+    for (size_t j = 0; j < array[i].size(); j++) {
+      std::cout << array[i][j] << ", ";
+    }
+  }
+  std::cout << "]" << std::endl;
+}
+
 /*
   https://en.wikipedia.org/wiki/Jacobsthal_number
 */
@@ -89,12 +99,13 @@ void sort_pairs(std::vector<int> &main, const long pair_size) {
     return;
   }
   for (long i = 0; i + pair_size < size; i += pair_size * 2) {
+    display_array(main);
     if (is_pair_greater(main, i, i + pair_size, pair_size)) {
       std::swap_ranges(main.begin() + i, main.begin() + i + pair_size, main.begin() + i + pair_size);
     }
   }
   sort_pairs(main, pair_size * 2);
-
+  display_array(main);
   std::vector<std::vector<int> > new_main; // b0 + a0...ax
   std::vector<int> tmp;
   std::vector<std::vector<int> > pend; // b1 + bx
@@ -103,30 +114,33 @@ void sort_pairs(std::vector<int> &main, const long pair_size) {
   new_main.push_back(tmp);
   copy_interval(main, new_main, pair_size, pair_size);
   copy_interval(main, pend, pair_size, pair_size * 2);
-
+  std::cout << "Pend: "; display_matrix(pend);
+  std::cout << "NewMain: "; display_matrix(new_main);
   /*
-        perv == 1
-        curr == 3
-        curr - perv == 2
-        interval_index == 0 - 1
-        perv == 3
-        curr == 5
-        interval_index = 2 - 5(curr - perv)
+    perv == 1
+    curr == 3
+    curr - perv == 2
+    interval_index == 0 - 1
+    perv == 3
+    curr == 5
+    interval_index = 2 - 5(curr - perv)
        b2 b3 b4
         0  1  2
     b1 a1 a2 a3 a4
      0  1  2  3  4
   */
+  /*
+    (b1 a1) (b2 a2) (b3 a3) (b4 a4) (b5 a5)
+  */
+
   long perv = 1;
   long curr = 3;
   long index = 0;
   while (curr - perv + perv - 2 < pend.size()) {
     for (long i = curr - perv + perv - 2; i >= perv - 1; --i) {
-      // std::cout << "i: " << i << std::endl;
-      // std::cout << "perv - 1: " << perv - 1 << std::endl;
       IT it = std::upper_bound(
         new_main.begin(),
-        new_main.begin() + curr - perv + perv - 2,
+        new_main.begin() + curr - perv + perv - 1,
         pend[i],
         comp_upper);
       new_main.insert(it, pend[i]);
@@ -136,13 +150,13 @@ void sort_pairs(std::vector<int> &main, const long pair_size) {
     curr = jacobsthal_numbers(curr + 1);
   }
   while (index < pend.size()) {
-    std::cout << "2\n";
     IT it = std::upper_bound(new_main.begin(), new_main.end(), pend[index], comp_upper);
     new_main.insert(it, pend[index]);
     ++index;
   }
 
   // copy everything from new_main to main
+  display_matrix(new_main);
   copy(new_main, main);
 }
 
@@ -156,8 +170,8 @@ void merge_insertion_sort(std::vector<int> &numbers)
 }
 
 int main(int ac, char **av) {
-  const long size = 8;
-  int numbers[] = {11,2, 17,0, 16,8, 6,15};
+  const long size = 5;
+  int numbers[] = {11, 2, 6, 7, 8};
   std::vector<int> input(&numbers[0], &numbers[size]);
   merge_insertion_sort(input);
 
